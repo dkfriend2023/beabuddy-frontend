d0b0f49edd4cdbab9f6970d8defbc40b43585ce2
sehyun <e6d1fe@gmail.com>
2023-07-16 19:52:55 +0900

    made AreaCard component

---
+++ src/components/AreaCard.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import sampleimg from "/public/sampleimg.png";
+import location from "/public/location.svg";
+
+function AreaCard() {
+  return (
+    <div className="w-[148px] h-[196px] bg-[#ffffff] rounded-[16px] shadow-[0_2px_8px_0_rgba(0,0,0,0.04)] p-[10px] flex flex-col justify-center items-center">
+      <Image src={sampleimg} width={90} height={90} alt="icon" />
+      <div className="text-[1f2937] text-[16px] font-semibold mt-[16px] mb-[3.98px]">
+        연세대학교 (신촌)
+      </div>
+      <div className="flex items-baseline justify-start gap-[5.7px]">
+        <Image src={location} width={8.3} height={10.95} alt="icon" />
+        <div className="text-[#6b7280] text-[10px] font-medium">
+          연세대 남문 ~ 신촌역 상권
+          <br />
+          연세대 서문 ~ 연남동 상권
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AreaCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
+import AreaCard from "../components/AreaCard";
 import NavBar from "../components/NavBar";
 
 function Page() {
   return (
     <div className="h-screen bg-[#f6f6f6] relative px-[18px] pt-[32px]">
       <Header />
+      <AreaCard />
       <NavBar />
     </div>
   );
