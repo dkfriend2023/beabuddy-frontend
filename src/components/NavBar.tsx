b661691429b2e8bf409f3392ede804cb4431717a
sehyun <e6d1fe@gmail.com>
2023-07-16 13:51:00 +0900

    made NavBar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import navhome from "/public/nav-icons/nav-home.svg";
+import navheart from "/public/nav-icons/nav-heart.svg";
+import navsearch from "/public/nav-icons/nav-search.svg";
+import navreserve from "/public/nav-icons/nav-reserve.svg";
+import navprofile from "/public/nav-icons/nav-search.svg";
+
+function NavBar() {
+  return (
+    <div className="bg-[#ffffff] w-full h-[64px] rounded-t-[20px] shadow-[0_-5px_14px_0_rgba(0,0,0,0.09)] absolute bottom-0 left-0 right-0 flex justify-evenly items-center">
+      <Link href="#">
+        <Image src={navhome} width={36} height={36} alt="icon" />
+      </Link>
+      <Link href="#">
+        <Image src={navheart} width={36} height={36} alt="icon" />
+      </Link>
+      <Link href="#">
+        <Image src={navsearch} width={36} height={36} alt="icon" />
+      </Link>
+      <Link href="#">
+        <Image src={navreserve} width={36} height={36} alt="icon" />
+      </Link>
+      <Link href="#">
+        <Image src={navprofile} width={36} height={36} alt="icon" />
+      </Link>
+    </div>
+  );
+}
+
+export default NavBar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import NavBar from "../components/NavBar";
+
 function Page() {
-  return <div className="">dkfriend main</div>;
+  return (
+    <div className="h-screen bg-[#f6f6f6] relative">
+      <NavBar />
+    </div>
+  );
 }
 
 export default Page;
