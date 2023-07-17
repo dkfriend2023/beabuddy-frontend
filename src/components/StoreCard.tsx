b12738ac9a537d8fe751f80e264bc6e5161e1c59
whale <lje1203@yonsei.ac.kr>
2023-07-17 21:52:18 +0900

    responsive screen

--- src/components/StoreCard.tsx
+++ src/components/StoreCard.tsx
@@ -5,13 +5,13 @@ import location from "/public/location.svg";
 
 function StoreCard() {
   return (
-    <div className="w-full h-[88px] bg-[#ffffff] rounded-[10px] shadow-[0_1px_4px_0_rgba(34,34,34,0.06)] p-[12px] flex justify-between items-center">
-      <Image src={samplestoreimg} width={64} height={64} alt="img" />
-      <div className="flex flex-col gap-[6px] items-baseline">
-        <div className="text-[#1f2937] text-[16px] font-semibold">크리스터치킨</div>
-        <div className="flex gap-[4px] items-center">
+    <div className="w-full h-[88px] sm:h-[70px] bg-[#ffffff] rounded-[10px] shadow-[0_1px_4px_0_rgba(34,34,34,0.06)] p-[12px] flex justify-between items-center">
+      <Image src={samplestoreimg} width={64} height={64} alt="img" className="sm:w-[40px] md:w-[55px]"/>
+      <div className="flex flex-col gap-[6px] sm:gap-[2px] sm:w-[100px] items-baseline">
+        <div className="text-[#1f2937] text-[16px] sm:text-[13px] font-semibold">크리스터치킨</div>
+        <div className="flex gap-[4px] items-center sm:hidden">
           <Image src={location} width={16} height={16} alt="icon" />
-          <div className="text-[#6b7280] text-[10px] font-normal">
+          <div className="text-[#6b7280] text-[10px] sm:w-[80px] font-normal">
             서대문구 연세로 11길 30
             <br />
             (창천동, 연세대학교 남문)
@@ -19,7 +19,7 @@ function StoreCard() {
         </div>
       </div>
       <div className="h-full flex items-end">
-        <div className="w-[88px] h-[28px] rounded-[8px] bg-[#fe8d00] text-[#fff] text-[12px] font-semibold flex justify-center items-center">
+        <div className="w-[88px] h-[28px] sm:w-[60px] rounded-[8px] bg-[#fe8d00] text-[#fff] text-[12px] font-semibold flex justify-center items-center">
           예약
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
+    screens: {
+      'sm': {'min': '250px', 'max': '350px'},
+      // => @media (min-width: 640px) { ... }
+
+      'md': {'min': '350px', 'max': '500px'},
+      // => @media (min-width: 768px) { ... }
+
+      'lg': {'min': '500px', 'max': '800px'},
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': {'min': '800px', 'max': '1000px'},
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': {'min': '1000px'},
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 };
