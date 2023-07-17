f32a02bada017723e103e36e3f1b7e1015f9683b
whale <lje1203@yonsei.ac.kr>
2023-07-17 17:30:09 +0900

    searchbar click area edit

--- src/components/AdCard.tsx
+++ src/components/AdCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function AdCard() {
     return (
-        <div className="w-[280px] h-[120px] bg-[#ff9f06] rounded-[28px] shadow-[0_2px_8px_0_rgba(0,0,0,0.04)] mb-[18px]"></div>
+        <div className="w-[280px] h-[120px] bg-[#ff9f06] shadow-[0_2px_8px_0_rgba(0,0,0,0.04)] mb-[18px] flex flex-col justify-center items-center"></div>
     );
   }
   
