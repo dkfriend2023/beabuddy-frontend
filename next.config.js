e3e8f94416be5342dab287660fb86d6cbdf3a8e5
sehyun <e6d1fe@gmail.com>
2024-05-15 12:15:29 +0900

    feat: get image from api

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {};
 
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "beerbuddy2023.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
--- src/components/AreaCard.tsx
+++ src/components/AreaCard.tsx
@@ -4,7 +4,7 @@ import sampleimg from "/public/sampleimg.png";
 import location from "/public/location.svg";
 
 interface Area {
-  image: string | null;
+  image: string;
   name: string;
   description: string;
 }
@@ -12,15 +12,20 @@ interface Area {
 const AreaCard: React.FC<Area> = ({ image, name, description }) => {
   return (
     <div className="min-h-[196px] w-[170px] bg-[#ffffff] rounded-[16px] shadow-[0_2px_8px_0_rgba(0,0,0,0.04)] p-[10px] flex flex-col justify-center items-center">
-      <Image src={sampleimg} width={90} height={90} alt="icon" />
+      <Image
+        src={`${process.env.NEXT_PUBLIC_DB_HOST}${image}`}
+        width={90}
+        height={90}
+        alt="icon"
+      />
       <div className="text-[1f2937] text-[16px] font-semibold mt-[16px] mb-[3.98px]">
         {name}
       </div>
-      <div className="flex items-baseline justify-start gap-[5.7px]">
-        {/* <Image src={location} width={8.3} height={33} alt="icon" /> */}
-        {/* <div className="text-[#6b7280] text-[10px] font-medium max-w-[130px]">
+      <div className="flex items-center justify-start gap-[5.7px]">
+        <Image src={location} width={16} height={16} alt="icon" />
+        <div className="text-[#6b7280] text-[11px] font-medium max-w-[130px]">
           {description}
-        </div> */}
+        </div>
       </div>
     </div>
   );
